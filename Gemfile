9b16fd808bc160f9fa717ee3e14be057a34ab73d
Olesya Bar <2olesya.baranova@gmail.com>
2023-06-10 22:40:33 -0400

    Adding filtering / search to Events view via bootstrap-table

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 gem "cssbundling-rails", "~> 1.1"
 
 gem "jsbundling-rails", "~> 1.1"
+
+gem "bootstrap-table-rails"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+//= require bootstrap-table/bootstrap-table
