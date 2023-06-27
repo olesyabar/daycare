5507d5362b0735dc808c310f461324b816b05290
Olesya Bar <2olesya.baranova@gmail.com>
2023-06-26 22:33:14 -0400

    Adding more CRUD for Guardians

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,6 +1,6 @@
 class GuardiansController < ApplicationController
 
-  before_action :set_guardian, only: [:show]
+  before_action :set_guardian, only: %i[ show edit update destroy ]
 
   def login
   end
@@ -13,9 +13,34 @@ class GuardiansController < ApplicationController
   end
 
   def new
+    @guardian = Guardian.new
+  end
+
+  def edit
   end
 
   def create
+    @guardian = Guardian.new(guardian_params)
+    if @guardian.save
+      flash.notice = "Guardian was successfully created!"
+      redirect_to @guardian
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @guardian.update(guardian_params)
+      flash.notice = "Guardian was successfully updated!"
+      redirect_to @guardian
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @guardian.destroy
+    flash.notice = "Guardian was successfully deleted!"
   end
 
   private
